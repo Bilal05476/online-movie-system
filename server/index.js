a8ab0224f283789488dda4066a6d43c3a54870c2
bilal05476 <ba4762313@gmail.com>
2023-09-19 20:09:36 +0500

    express-server-setup

--- server/index.js
+++ server/index.js
@@ -1,12 +1,44 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
 import { prisma } from "./prisma/index.js";
-console.log("Hello World");
 
+const app = express();
+app.use(cors());
+
+const port = process.env.PORT || 8080;
+dotenv.config();
+
+app.use(
+  bodyParser.json({
+    limit: "250mb",
+  })
+);
+
+app.use(
+  bodyParser.urlencoded({
+    limit: "250mb",
+    parameterLimit: 100000,
+    extended: true,
+  })
+);
+
+app.use("/", (req, res) => {
+  res.status(200).json({
+    message: `Server Running!`,
+  });
+});
+let variable;
 async function main() {
-  const actor = await prisma.actor.create({
+  variable = await prisma.actor.create({
     data: {
       name: "Bilal Ahmed",
     },
   });
-  console.log(actor);
 }
 main();
+
+app.listen(port, () => {
+  console.log(`${variable}\nServer Running at port: ${port}`);
+});
